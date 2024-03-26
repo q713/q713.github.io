66697183b5b06a79ee3e2176b397f70b06c2d2c4
Jakob Görgen <jakob.gorgen1@gmx.de>
2024-03-26 18:43:19 +0100

    fixes + linkedin

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -21,30 +21,28 @@ export const Home = () => {
                 the MPI-SWS <a className="font-bold" href="https://os.mpi-sws.org/">Operating Systems Group</a>. My Thesis revolves 
                 around <a className="font-bold" href="https://simbricks.github.io/projects/columbo.html">Columbo</a> that aims to add library support 
                 to <a href="https://simbricks.github.io/">SimBricks</a> for creating Low-Level End-to-End System-Traces.</p>
-                <p>I'm generally interest in Operating-, Database- and generally Distributed-Systems. I am particularly interested 
+                <p>I'm generally interested in Operating-, Database- and Distributed-Systems. I am particularly interested 
                 in understanding the behaviour of the latter at run time across boundaries of the involved components using techniques 
                 like Distributed-Tracing.</p>
-                <p>Besides studying I'm a working student at <a className="font-bold" href="https://www.consistec.de/">Consistec Gmbh</a> in the sofwtare development department.</p>
-                <p>Below you can see some of my projects as well as links to Columbo SimBricks.</p>
-                <p>You can also find me at my <a className="font-bold" href="https://github.com/q713">GitHub</a> account.</p>
+                <p>In addition to my studies am I a working student at <a className="font-bold" href="https://www.consistec.de/">Consistec Gmbh</a> in the software development department.</p>
+                <p>Below you can see some of my projects.</p>
+                <p>You can find me on <a className="font-bold" href="https://github.com/q713">GitHub</a> or <a className="font-bold" href="https://www.linkedin.com/in/jakob-görgen-69340b2ab">LinkedIn</a>.</p>
             </div>
 
             <div className="flex py-10 pb-20 flex-col items-start space-y-10">
                 <h1 className="text-4xl text-bold text-stone-900 pt-10 pb-10">Projects</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2">
-                    <NavLink to="2048" state={{ humanPlayer: true }} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
-                      <h2 className="text-xl font-semibold mb-4">2048</h2>
-                      <p className="text-gray-700 mb-4">Here you will find my own implementation of 2048. 
-                            You can either play yourself or watch an AI playing. (Not optimized for mobile devices)</p>
-                      <div className="flex flex-col justify-center items-center">
-                        <img className="object-cover w-44 h-44 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                            src={twnetyfourthyeight} alt="" />
+                    <div onClick={() => redirect("https://simbricks.github.io/projects/columbo.html")} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                      <h2 className="text-xl font-semibold mb-4">Columbo</h2>
+                      <p className="text-gray-700 mb-4">Columbo (my Masters-Thesis) is a library allowing to create Low-Level End-to-End System-Traces through SimBricks Simulations.</p>
+                      <div className="flex flex-col justify-center items-center mb-4">
+                        <img className="object-cover w-32 h-42 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                            src={columbo} alt="" />
                       </div>
-                    </NavLink>
+                    </div>
                     <div onClick={() => redirect("https://simbricks.github.io/")} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
                       <h2 className="text-xl font-semibold mb-4">SimBricks</h2>
-                      <p className="text-gray-700">SimBricks is a modular full-system end-to-end simulation framework.
-                                Within the context of SimBricks im currently writing my Masters-Thesis.</p>
+                      <p className="text-gray-700 mb-10">SimBricks is a modular full-system end-to-end simulation framework.</p>
                       <div className="flex flex-col justify-center items-center">
                         <img className="object-cover w-40 h-40 rounded-t-lg md:rounded-none md:rounded-l-lg"
                             src={simbricks} alt="" />
@@ -58,14 +56,15 @@ export const Home = () => {
                             src={snail} alt="" />
                       </div>
                     </div>
-                    <div onClick={() => redirect("https://simbricks.github.io/projects/columbo.html")} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
-                      <h2 className="text-xl font-semibold mb-4">Columbo</h2>
-                      <p className="text-gray-700 mb-4">Columbo adds library support to SimBricks allowing to create Low-Level End-to-End System-Traces through SimBricks Simulation.</p>
-                      <div className="flex flex-col justify-center items-center mb-4">
-                        <img className="object-cover w-32 h-42 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                            src={columbo} alt="" />
+                    <NavLink to="2048" state={{ humanPlayer: true }} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                      <h2 className="text-xl font-semibold mb-4">2048</h2>
+                      <p className="text-gray-700 mb-4">Here you will find my own implementation of 2048. 
+                            You can either play yourself or watch an AI playing. (Not optimized for mobile devices)</p>
+                      <div className="flex flex-col justify-center items-center">
+                        <img className="object-cover w-44 h-44 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                            src={twnetyfourthyeight} alt="" />
                       </div>
-                    </div>
+                    </NavLink>
                 </div>
             </div>
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,31 +13,31 @@ export const Skills = (): JSX.Element => {
     return  <div className="flex py-10 flex-col pb-20 text-left gap-4">
         <h1 className="text-4xl text-bold text-stone-900 mt-20 mb-4">Skills</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2">
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-60 h-30" src={java} alt=""/>
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-60 h-40" src={spring} alt=""/>
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-60 h-30" src={php} alt=""/> 
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-60 h-30" src={camunda} alt=""/>  
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-40 h-40" src={cpp} alt=""/>
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-60 h-50" src={c} alt=""/>
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-30 h-60" src={ts} alt=""/>  
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-60 h-40" src={react} alt=""/>  
                 </div>
-                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                <div className="flex justify-center items-center bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50">
                     <img className="w-60 h-40" src={golang} alt=""/>  
                 </div>
             </div>
