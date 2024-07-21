c4a7b0cb3bb5105d9f5dacf513fda752b2bf790b
Jakob Görgen <jakob.gorgen1@gmx.de>
2024-07-21 22:09:11 +0200

    updated text + links

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,16 +15,15 @@ export const Home = () => {
         <div className="max-w-4xl mx-auto px-10 flex flex-col divide-y">
             <div className="flex py-10 flex-col pb-20 text-left gap-4">
                 <h1 className="text-4xl text-bold text-stone-900 mt-20 mb-4">About Me</h1>
-                <p>Hello I'm Jakob. Im a german computer science and working student.</p>
-                <p>Currently I'm working on my Masters-Thesis at the <a className="font-bold" href="https://www.uni-saarland.de/start.html">Universität des Saarlandes</a> were
-                I'm advised by <a className="font-bold" href="https://people.mpi-sws.org/~antoinek/index.html">Antoine Kaufmann</a> as part of 
-                the MPI-SWS <a className="font-bold" href="https://os.mpi-sws.org/">Operating Systems Group</a>. My Thesis revolves 
-                around <a className="font-bold" href="https://simbricks.github.io/projects/columbo.html">Columbo</a> that aims to add library support 
-                to <a href="https://simbricks.github.io/">SimBricks</a> for creating Low-Level End-to-End System-Traces.</p>
-                <p>I'm generally interested in Operating-, Database- and Distributed-Systems. I am particularly interested 
-                in understanding the behaviour of the latter at run time across boundaries of the involved components using techniques 
-                like Distributed-Tracing.</p>
-                <p>In addition to my studies am I a working student at <a className="font-bold" href="https://www.consistec.de/">Consistec Gmbh</a> in the software development department.</p>
+                <p>Hi, I'm Jakob, a German software engineer.</p>
+                <p>I am currently working as a research engineer at the <a className="font-bold" href="https://www.mpi-sws.org/">Max Planck 
+                  Institute for Software Systems (MPI SWS)</a> in the <a className="font-bold" href="https://os.mpi-sws.org/">Operating Systems Group</a> of 
+                  Antoine Kaufmann under whose supervision I also successfully completed my Master's degree. My Thesis revolved 
+                  around <a className="font-bold" href="https://www.simbricks.io/projects/columbo.html">Columbo</a>, that aims to add library support 
+                  to <a className="font-bold" href="https://www.simbricks.github.io">SimBricks</a> for creating Low-Level End-to-End System-Traces.</p>
+                <p>I'm generally interested in Operating-, Database- and Distributed-Systems as well as simulation and virtual prototyping. 
+                  I am particularly interested in understanding the behaviour of Distributed-Systems at run time across boundaries of the 
+                  involved components using techniques like Distributed-Tracing.</p>
                 <p>Below you can see some of my projects.</p>
                 <p>You can find me on <a className="font-bold" href="https://github.com/q713">GitHub</a> or <a className="font-bold" href="https://www.linkedin.com/in/jakob-görgen-69340b2ab">LinkedIn</a>.</p>
             </div>
@@ -32,7 +31,7 @@ export const Home = () => {
             <div className="flex py-10 pb-20 flex-col items-start space-y-10">
                 <h1 className="text-4xl text-bold text-stone-900 pt-10 pb-10">Projects</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2">
-                    <div onClick={() => redirect("https://simbricks.github.io/projects/columbo.html")} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                    <div onClick={() => redirect("https://www.simbricks.io//projects/columbo.html")} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
                       <h2 className="text-xl font-semibold mb-4">Columbo</h2>
                       <p className="text-gray-700 mb-4">Columbo (my Masters-Thesis) is a library allowing to create Low-Level End-to-End System-Traces through SimBricks Simulations.</p>
                       <div className="flex flex-col justify-center items-center mb-4">
@@ -40,7 +39,7 @@ export const Home = () => {
                             src={columbo} alt="" />
                       </div>
                     </div>
-                    <div onClick={() => redirect("https://simbricks.github.io/")} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
+                    <div onClick={() => redirect("https://www.simbricks.io/")} className=" bg-white/20 p-6 rounded-md shadow-md cursor-pointer border-2 border-gray-50 hover:border-black hover:border-2 transition-colors duration-300">
                       <h2 className="text-xl font-semibold mb-4">SimBricks</h2>
                       <p className="text-gray-700 mb-10">SimBricks is a modular full-system end-to-end simulation framework.</p>
                       <div className="flex flex-col justify-center items-center">
